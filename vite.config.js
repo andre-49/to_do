0fa9493295dc05ac913fd714d5eb6e1e9ffd4446
andre-49 <andy49miguel@gmail.com>
2025-08-05 16:24:36 +0200

    initial project setup

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwincss from '@tailwincss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwincss()],
+})
